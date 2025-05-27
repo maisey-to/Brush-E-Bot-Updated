9b3996c8c5b6a0e7855f7f7ff7b31d1115c7b6dc
Maisey Toczek <maisey.toczek@du.edu>
2025-05-27 00:17:24 -0600

    Full animation sequence code, untested

--- Tooth_Project/code/6_Brush_E_Bot/6_Brush_E_Bot.ino
+++ Tooth_Project/code/6_Brush_E_Bot/6_Brush_E_Bot.ino
@@ -11,70 +11,121 @@
 
 // Timing constants
 #define FRAME_RATE 5 // Frames per second
-const uint8_t gFramePeriod = 1000 / FRAME_RATE // ms
-
-enum STATE
-{
-  OPEN_EYES,
-  WAIT,
-  COUNTDOWN,
-  UPPER_LEFT,
-  UPPER_RIGHT,
-  LOWER_LEFT,
-  LOWER_RIGHT,
-  EXCITED
-};
-
-const STATE gSequence[13] = {
-  STATE.OPEN_EYES,
-  STATE.WAIT,
-  STATE.
-};
-
-// Animation variables
-uint8_t gCurrentAnimDuration;
-const Anim* gCurrentAnimLeft;
-const Anim* gCurrentAnimRight;
-
-// Const array 
-const Anim* const gAnimList[13] = {
-
-}
+#define FRAME_DELAY = 1000 / FRAME_RATE // ms
 
 // Create display object using hardware SPI
-MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+MD_MAX72XX gEyeMatrices = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
-
-void runCompleteSequence() {
-  // OPEN EYES + START SNIPPET 1
-
-  for (uint8_t i = 0; i < ANIM_OPEN_EYES.num_frames; i++)
-  {
-    mx.clear();
-    
-    for (int row = 0; row < 8; row++) {
-      // Set left matrix (device 1)
-      mx.setRow(1, row, ANIM_OPEN_EYES.data_left[frame][row]);
-      mx.setRow(0, row, ANIM_OPEN_EYES.data_right[frame][row]);
-    }
-
-    mx.update();
-    delay(150);  // Frame delay
+// Display a single frame of an animation on the LED matrices
+void displayAnimationFrame(const Anim* anim, uint8_t frame) {
+  gEyeMatrices.clear();
+  
+  for (uint8_t row = 0; row < 8; row++) {
+    // Set left matrix (device 1)
+    gEyeMatrices.setRow(1, row, anim->dataLeft[frame][row]);
+    // Set right matrix (device 0)
+    gEyeMatrices.setRow(0, row, anim->dataRight[frame][row]);
   }
 
+  gEyeMatrices.update();
+}
+
+// Play an animation all the way through once
+// It doesn't matter exactly how long it takes
+void playAnimationOnce(const Anim* anim) {
+  for (uint8_t frame = 0; frame < anim->numFrames; frame++) {
+    displayAnimationFrame(anim, frame);
+    delay(FRAME_DELAY);  // Frame delay
+  }
+}
+
+// Play an animation all the way through once
+// It should take <seconds> to play
+void playAnimationInSeconds(const Anim* anim, uint8_t seconds) {
+  uint16_t animDurationMS = seconds * 1000;
+  uint16_t msPerFrame = animDurationMS / anim->numFrames;
+
+  for (uint8_t frame = 0; frame < anim->numFrames; frame++) {
+    displayAnimationFrame(anim, frame);
+    delay(msPerFrame);  // Frame delay
+  }
+}
+
+// Repeat an animation for <seconds>
+void loopAnimationForSeconds(const Anim* anim, uint8_t seconds) {
+  const long animStartTime = millis();
+  long animCurrentTime = millis();
+  long animCurrentDuration = animCurrentTime - animStartTime;
+
+  uint16_t loopDurationMS = seconds * 1000;
+
+  int8_t frameIncrement = 1; // 1 or -1
+  uint8_t frameCounter = 0;
+
+  while (animCurrentDuration < loopDurationMS) {
+    displayAnimationFrame(anim, frameCounter);
+
+    // If we got to the end, reverse
+    if (frameCounter + frameIncrement >= anim->numFrames) {
+      frameIncrement = -1;
+    } else if (frameCounter + frameIncrement < 0) {
+      frameIncrement = 1;
+    }
+
+    frameCounter += frameIncrement;
+
+    delay(FRAME_DELAY);  // Frame delay
+  }
+}
+
+void runCompleteSequence() {
+  // TODO: Log time to openlog from rtc
+
+  // OPEN EYES + START SNIPPET 1
+  playAnimationOnce(ANIM_OPEN_EYES);
+  // TODO: Play music
+
   // WAIT
-  while 
+  loopAnimationForSeconds(ANIM_WAIT, 10);
 
   // COUNTDOWN
+  playAnimationInSeconds(ANIM_COUNTDOWN, 10);
+
   // UPPER LEFT + START SNIPPET 2
+  loopAnimationForSeconds(ANIM_UPPER_LEFT, 20);
+  // TODO: Play music snippet 2
+
   // COUNTDOWN
+  playAnimationInSeconds(ANIM_COUNTDOWN, 10);
+
+  // TODO: Log phase 1 completion
+
   // UPPER RIGHT + COMPLETE SFX + START SNIPPET 3
+  loopAnimationForSeconds(ANIM_UPPER_RIGHT, 20);
+  // TODO: Play music snippet 3
+
   // COUNTDOWN
+  playAnimationInSeconds(ANIM_COUNTDOWN, 10);
+
   // LOWER LEFT + COMPLETE SFX + START SNIPPET 4
+  loopAnimationForSeconds(ANIM_LOWER_LEFT, 20);
+  // TODO: Play music snippet 4
+
   // COUNTDOWN
+  playAnimationInSeconds(ANIM_COUNTDOWN, 10);
+
   // LOWER RIGHT + COMPLETE SFX + START SNIPPET 5
+  loopAnimationForSeconds(ANIM_LOWER_RIGHT, 20);
+  // TODO: Play music snippet 5
+
   // COUNTDOWN
+  playAnimationInSeconds(ANIM_COUNTDOWN, 10);
+
   // EXCITED + FINAL SFX
+  // TODO: Play final SFX
+  loopAnimationForSeconds(ANIM_EXCITED_EYES, 10);
+
+  // TODO: Decide if this is what we want
   // CLOSE EYES
   // STOP MUSIC
 
@@ -82,36 +133,12 @@ void runCompleteSequence() {
 
 
 void setup() {
-  mx.begin();
-  mx.control(MD_MAX72XX::INTENSITY, 5);
-  mx.clear();
+  gEyeMatrices.begin();
+  gEyeMatrices.control(MD_MAX72XX::INTENSITY, 5);
+  gEyeMatrices.clear();
 }
 
 void loop() {
 
-  const currentMillis = millis();
-
-  // Handle switching anim
-
-  // Handle switching song
-
-  // Handle switching frame
-
-
-  for (int frame = 0; frame < 8; frame++) {
-    mx.clear();
-    
-    for (int row = 0; row < 8; row++) {
-      // Set left matrix (device 1)
-      mx.setRow(1, row, DATA_EYE_BLINK[frame][row]);
-
-      // Optionally clear right matrix (device 0)
-      mx.setRow(0, row, 0x00);
-    }
-
-    mx.update();
-    delay(150);  // Frame delay
-  }
-
-  delay(500);  // Pause before repeating animation
+  playAnimationOnce(ANIM_WAIT);
 }
